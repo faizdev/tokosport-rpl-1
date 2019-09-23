24af99d5b9c1b2a13f335063dd2a28e987c88b85
Alfian Faiz <a.faiz@students.unnes.ac.id>
2019-09-23 11:33:15 +0700

    inisialisasi awal project

---
+++ resources/views/about-me.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>About Me</title>
+</head>
+<body>
+    <h1>INI HALAMAN BIODATA</h1>
+    ini berubah
+    <table>
+        <tr>
+            <td>Nama</td>
+            <td>Alfian Faiz</td>
+        </tr>
+        <tr>
+            <td>Kelas</td>
+            <td>RPL 1</td>
+        </tr>
+        <tr>
+            <td>Hobby</td>
+            <td>Game</td>
+        </tr>
+    </table>
+</body>
+</html>
